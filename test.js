6fe7d067d3197f06de98a145b5d25af0ecda3515
Vighnesh <shreeraghvr2002@gmail.com>
2025-01-08 10:56:56 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+// Simple JavaScript code to print a message and perform a basic calculation
+console.log("Hello, World!");
+
+// Function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Example usage
+const num1 = 5;
+const num2 = 7;
+console.log(`The sum of ${num1} and ${num2} is: ${addNumbers(num1, num2)}`);
